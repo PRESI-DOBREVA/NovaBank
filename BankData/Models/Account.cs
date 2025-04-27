9d3c03944da2ff8c27201a03d8091b9c28d59050
Isonski <elicasotirova6@gmail.com>
2025-04-27 11:34:33 +0300

    final final smyly updates

--- BankData/Models/Account.cs
+++ BankData/Models/Account.cs
@@ -16,7 +16,7 @@ namespace BankData.Models
 
         public int CustomerId { get; set; }
 
-        public virtual Customer Customer { get; set; };
+        public virtual Customer Customer { get; set; }
 
         public virtual ICollection<Card> Cards { get; set; }
 
