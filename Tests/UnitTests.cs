ee3c7292e0bb51059a41a78bd3d5763158192222
Преслава Добрева <preslava.dobreva.2007@gmail.com>
2025-05-10 08:55:21 +0300

    Още тестове и от мен  и промяна на дизайна в Accounts/Create

--- Tests/UnitTests.cs
+++ Tests/UnitTests.cs
@@ -48,7 +48,7 @@
                 Id = 1,
                 Amount = 1000,
                 AccountId = 1,
-                TransactionType = 2
+                TransactionType = "Transfer"
             };
 
             // Act
@@ -92,6 +92,77 @@
             var result = customer.Name;
             // Assert
             Assert.AreEqual("John Doe", result);
+        }
+        [TestMethod]
+        public void BankAccountBalanceTest()
+        {
+            // Arrange
+            var account = new BankData.Models.Account
+            {
+                Id = 1,
+                CustomerId = 1,
+                Balance = 1000
+            };
+
+            // Act
+            var result = account.Balance;
+
+            // Assert
+            Assert.AreEqual(1000, result);
+        }
+        [TestMethod]
+        public void CardTypeTest()
+        {
+            // Arrange
+            var card = new BankData.Models.Card
+            {
+                Id = 1234567,
+                AccountId = 1,
+                Type = "MasterCard"
+            };
+
+            // Act
+            var result = card.Type;
+
+            // Assert
+            Assert.AreEqual("MasterCard", result);
+        }
+        [TestMethod]
+        public void LoanInterestTest()
+        {
+            // Arrange
+            var loan = new BankData.Models.Loan
+            {
+                Id = 1,
+                CustomerId = 1,
+                Amount = 5000,
+                Interest = 5.0m,
+                Term = 12
+            };
+
+            // Act
+            var result = loan.Interest;
+
+            // Assert
+            Assert.AreEqual(5.0m, result);
+        }
+        [TestMethod]
+        public void TransactionAccountIdTest()
+        {
+            // Arrange
+            var transaction = new BankData.Models.Transaction
+            {
+                Id = 1,
+                Amount = 1000,
+                AccountId = 1,
+                TransactionType = "Deposit"
+            };
+
+            // Act
+            var result = transaction.AccountId;
+
+            // Assert
+            Assert.AreEqual(1, result);
         }
     }
 }
